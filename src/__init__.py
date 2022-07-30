0f0dafdb53402d19c4cc7f79c56e906d446a9008
Nutt Chokwittaya <chokwittaya.nutt@gmail.com>
2022-07-30 14:30:54 -0500

    [3] - using Blueprint()

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,9 @@
 # factory file
 from flask import Flask
 import os
+from src.auth import auth
+from src.bookmarks import bookmarks
+
 
 def create_app(test_config=None):
 
@@ -13,12 +16,7 @@ def create_app(test_config=None):
     else:
         app.config.from_mapping(test_config)
 
-    @app.get('/')
-    def index():
-        return 'hello world'
-
-    @app.get('/hello')
-    def say_hello():
-        return {'message': 'Hello World'}
+    app.register_blueprint(auth)
+    app.register_blueprint(bookmarks)
 
     return app
